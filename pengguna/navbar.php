439efc5d21bc624a2da2eabc9844f520f54b4ea0
faishal15 <faishal15@gmail.com>
2017-05-15 18:10:22 +0700

    for merge

--- pengguna/navbar.php
+++ pengguna/navbar.php
@@ -7,12 +7,16 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="..\index.php"><span>TCARI</span></a>
+<!-- 			<a class="navbar-brand" href="..\index.php"><span>TCARI</span></a>
+ -->
+ 			<a class="navbar-brand" href="..\index.php"><img src="..\img/logo2.png" alt="" height="137%" align="
+			top" />
+                    </a>
 			<ul class="user-menu">
 				<li class="dropdown pull-right">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> <?php echo $_SESSION['uname'];?> <span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
-						<li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
+						<li><a href="index.php"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
 						<li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
 						<li><a href="..\logout.php"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
 					</ul>
